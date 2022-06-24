562e1f4d0cf63367fc7a2b6097562960726978ca
kisonhuang <916462938@qq.com>
2022-06-24 14:51:03 +0800

    格式化

--- src/test.ts
+++ src/test.ts
@@ -1,30 +1,30 @@
 // test.ts是karma.conf.js所需文件，用于递归加载所有.spec和框架文件。
 
 import 'zone.js/testing';
-import { getTestBed } from '@angular/core/testing';
+import {getTestBed} from '@angular/core/testing';
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
 // assert polyfill需要使用process。
 // 参考：https://github.com/browserify/commonjs-assert/blob/bba838e9ba9e28edf3127ce6974624208502f6bc/internal/assert/assertion_error.js#L138
 // 浏览器中使用的Node.js库timezone-mock需要assert polyfill。
 (globalThis as any).process = {
-  env: {},
+    env: {},
 };
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
-    <T>(id: string): T;
-    keys(): string[];
-  };
+    context(path: string, deep?: boolean, filter?: RegExp): {
+        <T>(id: string): T;
+        keys(): string[];
+    };
 };
 
 // 初始化Angular测试环境
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting(),
 );
 
 // 查找所有测试
