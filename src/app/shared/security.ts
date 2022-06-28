276c66b4b3a5083b43aebbee9e18d88fa856ed82
kisonhuang <916462938@qq.com>
2022-06-28 21:38:42 +0800

    整理toc

--- src/app/shared/security.ts
+++ src/app/shared/security.ts
@@ -1,17 +1,22 @@
 import {htmlFromStringKnownToSatisfyTypeContract} from 'safevalues/unsafe/reviewed';
 
+// 把指定元素的innerHTML转换为TrustedHTML
 export function fromInnerHTML(el: Element): TrustedHTML {
-    // SECURITY: Existing innerHTML content is already trusted.
+    // 安全性：现有innerHTML内容已经可信
+    // 执行unchecked转换：把满足TrustedHTML类型约定的字符串转换为TrustedHTML
     return htmlFromStringKnownToSatisfyTypeContract(el.innerHTML, '^');
 }
 
+// 把指定元素的outerHTML转换为TrustedHTML
 export function fromOuterHTML(el: Element): TrustedHTML {
-    // SECURITY: Existing outerHTML content is already trusted.
+    // 安全性：现有outerHTML内容已经可信
+    // 执行unchecked转换：把满足TrustedHTML类型约定的字符串转换为TrustedHTML
     return htmlFromStringKnownToSatisfyTypeContract(el.outerHTML, '^');
 }
 
+// 把SVG常量转换为TrustedHTML
 export function svg(constantSvg: TemplateStringsArray): TrustedHTML {
-    // SECURITY: Template literal argument with no interpolation is constant, and
-    // hence trusted.
+    // 安全性：没有插值的模板字面参数是常量，因此可信
+    // 执行unchecked转换：把满足TrustedHTML类型约定的字符串转换为TrustedHTML
     return htmlFromStringKnownToSatisfyTypeContract(constantSvg[0], '^');
 }
--- src/app/shared/toc.service.ts
+++ src/app/shared/toc.service.ts
@@ -17,8 +17,11 @@ export interface TocItem {
 
 @Injectable()
 export class TocService {
+
     tocList = new ReplaySubject<TocItem[]>(1);
+
     activeItemIndex = new ReplaySubject<number | null>(1);
+
     private scrollSpyInfo: ScrollSpyInfo | null = null;
 
     constructor(
@@ -54,60 +57,56 @@ export class TocService {
         this.scrollSpyInfo.active.subscribe(item => this.activeItemIndex.next(item && item.index));
     }
 
-    reset() {
-        this.resetScrollSpyInfo();
-        this.tocList.next([]);
-    }
-
-    // Transform the HTML content to be safe to use in the ToC:
-    //   - Strip off certain auto-generated elements (such as GitHub links and heading anchor links).
-    //   - Strip off any anchor links (but keep their content)
-    //   - Mark the HTML as trusted to be used with `[innerHTML]`.
+    // 把HTML转换为可以在ToC中安全使用的内容：
+    // + 删除自动生成的.github-links和.header-link元素及其内容
+    // + 把a元素的内容移到a元素的父节点中（添加到a元素前面），保留a元素的内容，删除a元素
+    // + 绕过安全性，把div.innerHTML认为是安全的HTML，转换为SafeHtml
     private extractHeadingSafeHtml(heading: HTMLHeadingElement) {
         const div: HTMLDivElement = this.document.createElement('div');
         div.innerHTML = unwrapHtmlForSink(fromInnerHTML(heading));
 
-        // Remove any `.github-links` or `.header-link` elements (along with their content).
+        // 删除自动生成的.github-links和.header-link元素及其内容
         div.querySelectorAll('.github-links, .header-link').forEach(link => link.remove());
 
-        // Remove any remaining `a` elements (but keep their content).
+        // 把a元素的内容移到a元素的父节点中（添加到a元素前面），保留a元素的内容，删除a元素
         div.querySelectorAll('a').forEach(anchorLink => {
-            // We want to keep the content of this anchor, so move it into its parent.
-            const parent = anchorLink.parentNode as Node;
+            const anchorParent = anchorLink.parentNode as Node;
             while (anchorLink.childNodes.length) {
-                parent.insertBefore(anchorLink.childNodes[0], anchorLink);
+                anchorParent.insertBefore(anchorLink.childNodes[0], anchorLink);
             }
-
-            // Now, remove the anchor.
             anchorLink.remove();
         });
 
         return {
-            // Security: The document element which provides this heading content is always authored by
-            // the documentation team and is considered to be safe.
+            // 绕过安全性，把div.innerHTML认为是安全的HTML，转换为SafeHtml
             content: this.domSanitizer.bypassSecurityTrustHtml(div.innerHTML.trim()),
             title: (div.textContent || '').trim(),
         };
     }
 
+    // 查找ToC标题
     private findTocHeadings(docElement: Element): HTMLHeadingElement[] {
         const headings = docElement.querySelectorAll<HTMLHeadingElement>('h1,h2,h3');
         const skipNoTocHeadings = (heading: HTMLHeadingElement) => !/(?:no-toc|notoc)/i.test(heading.className);
-
         return Array.prototype.filter.call(headings, skipNoTocHeadings);
     }
 
+    // 重置
+    reset() {
+        this.resetScrollSpyInfo();
+        this.tocList.next([]);
+    }
+
+    // 重置scrollSpyInfo
     private resetScrollSpyInfo() {
         if (this.scrollSpyInfo) {
             this.scrollSpyInfo.unspy();
             this.scrollSpyInfo = null;
         }
-
         this.activeItemIndex.next(null);
     }
 
-    // Extract the id from the heading; create one if necessary
-    // Is it possible for a heading to lack an id?
+    // 从h1~h6标题中提取id，id不存在时创建id
     private getId(h: HTMLHeadingElement, idMap: Map<string, number>) {
         let id = h.id;
         if (id) {
@@ -119,12 +118,13 @@ export class TocService {
         }
         return id;
 
-        // Map guards against duplicate id creation.
-        function addToMap(key: string) {
-            const oldCount = idMap.get(key) || 0;
-            const count = oldCount + 1;
-            idMap.set(key, count);
-            return count === 1 ? key : `${key}-${count}`;
+        // 把标题id添加到Map中，防止重复创建id，返回值：id、id-2...
+        function addToMap(hId: string) {
+            const oldCount = idMap.get(hId) || 0;
+            const newCount = oldCount + 1;
+            idMap.set(hId, newCount);
+            return newCount === 1 ? hId : `${hId}-${newCount}`;
         }
     }
+
 }
