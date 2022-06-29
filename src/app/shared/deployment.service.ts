9613a5b7b9ba6a6bc224d80dc71e2435e3e71ce3
kisonhuang <916462938@qq.com>
2022-06-29 09:02:22 +0800

    应用程序的部署信息

--- src/app/shared/deployment.service.ts
+++ src/app/shared/deployment.service.ts
@@ -3,16 +3,18 @@ import {LocationService} from 'app/shared/location.service';
 import {environment} from 'environments/environment';
 
 /**
- * Information about the deployment of this application.
+ * 应用程序的部署信息。
  */
 @Injectable()
 export class Deployment {
+
     /**
-     * The deployment mode set from the environment provided at build time;
-     * or overridden by the `mode` query parameter: e.g. `...?mode=archive`
+     * 构建时使用的环境中设置的部署模式，或者通过mode查询参数来覆盖，例如：...?mode=archive
      */
     mode: string = this.location.search().mode || environment.mode;
 
     constructor(private location: LocationService) {
+
     }
+
 }
