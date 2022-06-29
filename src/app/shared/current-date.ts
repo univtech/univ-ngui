33a6c5487ed5f02e02544362292ad0bd9324b221
kisonhuang <916462938@qq.com>
2022-06-29 08:25:31 +0800

    当前日期

--- src/app/shared/current-date.ts
+++ src/app/shared/current-date.ts
@@ -1,7 +1,13 @@
 import {InjectionToken} from '@angular/core';
 
+/**
+ * 当前日期令牌
+ */
 export const CurrentDateToken = new InjectionToken('CurrentDate');
 
+/**
+ * 当前日期提供器
+ */
 export function currentDateProvider() {
     return new Date();
 }
