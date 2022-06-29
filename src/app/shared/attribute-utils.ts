ff6d1e2e80fb375d8c4f791a18a0d1392a609d71
kisonhuang <916462938@qq.com>
2022-06-29 08:56:24 +0800

    处理HTML元素属性的工具

--- src/app/shared/attribute-utils.ts
+++ src/app/shared/attribute-utils.ts
@@ -1,58 +1,60 @@
-// Utilities for processing HTML element attributes
+// 处理HTML元素属性的工具
 import {ElementRef} from '@angular/core';
 
+/**
+ * HTML元素属性Map
+ */
 export interface AttrMap {
     [key: string]: string;
 }
 
 /**
- * Get attribute map from element or ElementRef `attributes`.
- * Attribute map keys are forced lowercase for case-insensitive lookup.
+ * 从HTML元素或HTML元素引用的attributes中获取属性，并转换为Map。<br>
+ * 因为查找不区分大小写，所以属性Map的Key强制转换为小写。
  *
- * @param el The source of the attributes.
+ * @param el 属性来源，HTML元素（HTMLElement）或HTML元素引用（ElementRef）
  */
 export function getAttrs(el: HTMLElement | ElementRef): AttrMap {
     const attrs: NamedNodeMap = el instanceof ElementRef ? el.nativeElement.attributes : el.attributes;
     const attrMap: AttrMap = {};
-    for (const attr of attrs as any as Attr[] /* cast due to https://github.com/Microsoft/TypeScript/issues/2695 */) {
+    // 转换原因：https://github.com/Microsoft/TypeScript/issues/2695
+    for (const attr of attrs as any as Attr[]) {
         attrMap[attr.name.toLowerCase()] = attr.value;
     }
     return attrMap;
 }
 
 /**
- * Return the attribute that matches `attr`.
+ * 获取属性值。
  *
- * @param attr Name of the attribute or a string of candidate attribute names.
+ * @param attrMap 属性Map
+ * @param attr 属性名
  */
-export function getAttrValue(attrs: AttrMap, attr: string | string[]): string | undefined {
-    const key = (typeof attr === 'string')
-        ? attr
-        : attr.find(a => attrs.hasOwnProperty(a.toLowerCase()));
-
-    return (key === undefined) ? undefined : attrs[key.toLowerCase()];
+export function getAttrValue(attrMap: AttrMap, attr: string | string[]): string | undefined {
+    const key = (typeof attr === 'string') ? attr : attr.find(a => attrMap.hasOwnProperty(a.toLowerCase()));
+    return (key === undefined) ? undefined : attrMap[key.toLowerCase()];
 }
 
 /**
- * Return the boolean state of an attribute value (if supplied).
+ * 把属性值转换为boolean类型
  *
- * @param attrValue The string value of some attribute (or undefined if attribute not present).
- * @param def Default boolean value when attribute is undefined.
+ * @param attrValue 属性值，属性未定义时为undefined
+ * @param defaultValue 属性未定义时的默认值
  */
-export function boolFromValue(attrValue: string | undefined, def: boolean = false) {
-    return attrValue === undefined ? def : attrValue.trim() !== 'false';
+export function boolFromValue(attrValue: string | undefined, defaultValue: boolean = false) {
+    return attrValue === undefined ? defaultValue : attrValue.trim() !== 'false';
 }
 
 /**
- * Return the boolean state of attribute from an element.
+ * 从HTML元素或HTML元素引用中获取指定属性的boolean值。
  *
- * @param el The source of the attributes.
- * @param atty Name of the attribute or a string of candidate attribute names.
- * @param def Default boolean value when attribute is undefined.
+ * @param el 属性来源，HTML元素（HTMLElement）或HTML元素引用（ElementRef）
+ * @param attr 属性名
+ * @param defaultValue 属性未定义时的默认值
  */
 export function getBoolFromAttribute(
     el: HTMLElement | ElementRef,
     attr: string | string[],
-    def: boolean = false): boolean {
-    return boolFromValue(getAttrValue(getAttrs(el), attr), def);
+    defaultValue: boolean = false): boolean {
+    return boolFromValue(getAttrValue(getAttrs(el), attr), defaultValue);
 }
