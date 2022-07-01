f114e0af9472fc07dea80f071b286d7ae8799736
kisonhuang <916462938@qq.com>
2022-07-01 11:34:52 +0800

    删除不需要的路径

--- tools/transforms/config.js
+++ tools/transforms/config.js
@@ -1,16 +1,12 @@
 const {resolve} = require('path');
 const {readdirSync} = require('fs');
 
-const PROJECT_ROOT = resolve(__dirname, '../../..');
-const AIO_PATH = resolve(PROJECT_ROOT, 'aio');
-const TEMPLATES_PATH = resolve(AIO_PATH, 'tools/transforms/templates');
-const API_TEMPLATES_PATH = resolve(TEMPLATES_PATH, 'api');
-const CONTENTS_PATH = resolve(AIO_PATH, 'content');
-const GUIDE_EXAMPLES_PATH = resolve(CONTENTS_PATH, 'examples');
-const SRC_PATH = resolve(AIO_PATH, 'src');
+const PROJECT_ROOT = resolve(__dirname, '../..');
+const TEMPLATES_PATH = resolve(PROJECT_ROOT, 'tools/transforms/templates');
+const CONTENTS_PATH = resolve(PROJECT_ROOT, 'content');
+const SRC_PATH = resolve(PROJECT_ROOT, 'src');
 const OUTPUT_PATH = resolve(SRC_PATH, 'generated');
 const DOCS_OUTPUT_PATH = resolve(OUTPUT_PATH, 'docs');
-const API_SOURCE_PATH = resolve(PROJECT_ROOT, 'packages');
 
 function requireFolder(dirname, folderPath) {
     const absolutePath = resolve(dirname, folderPath);
@@ -21,15 +17,10 @@ function requireFolder(dirname, folderPath) {
 
 module.exports = {
     PROJECT_ROOT,
-    AIO_PATH,
     TEMPLATES_PATH,
-    API_TEMPLATES_PATH,
     CONTENTS_PATH,
-    GUIDE_EXAMPLES_PATH,
     SRC_PATH,
     OUTPUT_PATH,
     DOCS_OUTPUT_PATH,
-    API_SOURCE_PATH,
     requireFolder
 };
-
