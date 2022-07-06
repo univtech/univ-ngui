5edd84dccefa473168213e390038faa787598333
kisonhuang <916462938@qq.com>
2022-07-06 11:26:02 +0800

    删除custom-search-defs

--- tools/transforms/angular-base-package/inline-tag-defs/custom-search-defs/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'searchKeywords',
-    description: '使用检索词创建元素的缩写。用法：`{@searchKeywords term1 term2 termN }`',
-    handler: function (doc, tagName, tagDescription) {
-        doc.searchKeywords = tagDescription;
-        return '';
-    }
-};
